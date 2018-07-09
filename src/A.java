d7225e719d61efd7f2be96c04d19f1f0afb4e735
Saiamol <Saiamol@Saiamol-PC>
2018-07-09 13:11:58 +0530

    just adding

---
+++ src/A.java
@@ -0,0 +1,17 @@
+
+
+public class A {
+	void functiont ()
+	{
+		System.out.println(+10);
+		System.out.println(+20);
+	}
+
+	public static void main(String[] args) {
+	int x=10;
+	A a1= new A();
+	System.out.println(x);
+	System.out.println(a1);
+	}
+
+}
